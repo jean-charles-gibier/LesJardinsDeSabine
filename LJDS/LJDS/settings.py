a4e359769a97daa8517ba0fd9359b41c62293a4d
Jean-Charles Gibier <jeancharlesg@free.fr>
2020-11-23 22:02:35 +0100

    Modifications front npm and else

--- LJDS/LJDS/settings.py
+++ LJDS/LJDS/settings.py
@@ -13,10 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os.path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 print("===> {} ".format(os.path.join(BASE_DIR, 'templates')))
 
@@ -30,7 +28,6 @@ SECRET_KEY = 'r=q5v(8y8&7bsy5_3_+j$lkexwdrv%%8t#9%+%ne+&6ybj)it='
 DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "15.237.146.152"]
-
 
 # Application definition
 
@@ -73,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'LJDS.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -83,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -103,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -117,7 +111,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -125,3 +118,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'dumps')
