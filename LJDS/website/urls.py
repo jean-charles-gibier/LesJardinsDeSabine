be57d18bef3dd60804c92ccf93f3f757a1941d74
Jean-Charles Gibier <jeancharlesg@free.fr>
2020-11-30 19:16:32 +0100

    Avant/apres Prez #10

--- LJDS/website/urls.py
+++ LJDS/website/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
-
 from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='LJDS/index.html'), name='index'),
+    path('avant_apres', TemplateView.as_view(template_name='LJDS/comparison.html'), name='comparison'),
+
 ]
